4f8e244b7441bf64d18f1ec65185cd55594d26d3
unknown <laween312@gmail.com>
2023-01-26 19:37:16 +0000

    adds break

--- milestone_3.py
+++ milestone_3.py
@@ -4,7 +4,7 @@ while True:
     
     if len("guess")==1 and guess.isalpha():
         print ("Good guess")
-    
+        break  
     else:
         print("Invalid letter. Please, enter a single alphabetical character.") 
         
